421d583200ec50d620ba05d8a1efeff1f835e1f3
Liseth Cardozo <cardozoliseth@gmail.com>
2017-07-31 16:27:10 -0700

    Solve string_char problem

--- string_chars.py
+++ string_chars.py
@@ -1,2 +1,15 @@
-# Write a program that takes a list of strings and a string containing a single character, and 
+# Write a program that takes a list of strings and a string containing a single character, and
 # prints a new list of all the strings containing that character.
+# test case:
+# # input
+# word_list = ['hello','world','my','name','is','Anna']
+# char = 'o'
+# # output
+# new_list = ['hello','world']
+word_list = ['hello','world','my','name','is','Anna']
+char = 'a'
+new_list = []
+for i in word_list:
+    if i.count(char) >= 1:
+        new_list += [i]
+print new_list
