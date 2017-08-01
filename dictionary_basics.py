d5f31e5666d191b1eb28ea57d8d2d5fd2b1615ce
Liseth Cardozo <cardozoliseth@gmail.com>
2017-07-31 22:03:12 -0700

    solve dictionary problem

---
+++ dictionary_basics.py
@@ -0,0 +1,16 @@
+# Assignment: Making and Reading from Dictionaries
+# Create a dictionary containing some information about yourself.
+# The keys should include name, age, country of birth, favorite language.
+
+# Write a function that will print something like the following as it executes:
+my_info = {
+"name": "Liseth",
+"age": 27,
+"country": "Bolivia",
+"favority_lenguage": "Ruby"
+}
+
+print "My name is:", my_info["name"]
+print "My age is:",  my_info["age"]
+print "My country is:",  my_info["country"]
+print "My favority lenguage is:", my_info["favority_lenguage"]
